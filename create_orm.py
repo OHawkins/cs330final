44f60316095ac3c0c9bd3bb50778f943235d3f74
OHawkins <ohawk7@gmail.com>
2017-05-11 09:38:43 -0500

    Changed to populate Heroku database

--- create_orm.py
+++ create_orm.py
@@ -19,9 +19,16 @@ class Event(Base):
     end = Column(DateTime)
     location = Column(String)
 
+    def __init__(self, id, name, category, start, end, location):
+        self.id = id
+        self.name = name
+        self.category = category
+        self.start = start
+        self.end = end
+        self.location = location
+
     def __repr__(self):
         return "<Event({}, {}-{})>".format(self.name, self.start, self.end)
-
 
 class Location(Base):
     __tablename__ = 'location'
@@ -29,14 +36,22 @@ class Location(Base):
     city = Column(String)
     country = Column(String)
 
+    def __init__(self, id, city, country):
+        self.id = id
+        self.city = city
+        self.country = country
+
     def __repr__(self):
         return "<Location({}, {})>".format(self.city, self.country)
-
 
 class Category(Base):
     __tablename__ = 'category'
     id = Column(Integer, primary_key=True)
     name = Column(String)
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
 
     def __repr__(self):
         return "<Category({})>".format(self.name)
@@ -44,47 +59,48 @@ class Category(Base):
 
 # create the engine
 engine = create_engine('postgres://fzoxyvegfbbwwp:dcc7d0363278a31d092ec89d957cac2874f27c0caad1261ffb01092c3c933bb2@ec2-54-235-90-107.compute-1.amazonaws.com:5432/d1veuupffk4555', echo=True)
-session = sessionmaker(bind=engine)
+Session = sessionmaker(bind=engine)
 
-db = session()
+db = Session()
 
 Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
 
 
-# populate the databases
+# connection for knuth cities table
 conn = psycopg2.connect(user='hawkol01', dbname='world', host='knuth.luther.edu')
 data = conn.cursor()
 
-
 # populate the location table
 data.execute("""SELECT * FROM city;""")
-
 res = data.fetchall()
 
 for i in res:
     new_place = Location(id=i[0], city=i[1], country=i[2])
     db.add(new_place)
 
+#connection to knuth database for insertion
+conn2 = psycopg2.connect('postgres://fzoxyvegfbbwwp:dcc7d0363278a31d092ec89d957cac2874f27c0caad1261ffb01092c3c933bb2@ec2-54-235-90-107.compute-1.amazonaws.com:5432/d1veuupffk4555')
+data2 = conn2.cursor()
+
 # populate the event table
-data.execute("""SELECT count(*) from event;""")
-num = data.fetchall()
+data2.execute("""SELECT count(*) FROM event;""")
+num = data2.fetchall()
 
 def crevnt(name, category, start, end, location):
     new_event = Event(num, name, category, start, end, location)
     db.add(new_event)
     db.commit()
 
-# grab events from database
-data.execute("""SELECT * FROM event;""")
-events = data.fetchall()
+# grab events from database for index()
+data2.execute("""SELECT * FROM event;""")
+events = data2.fetchall()
 def show_events():
     return events
 
-
 # populate the category table
-data.execute("""SELECT count(*) FROM category;""")
-catnum = data.fetchall()
+data2.execute("""SELECT count(*) FROM category;""")
+catnum = data2.fetchall()
 
 def crt_ctgry(name):
     new_category = Category(catnum, name)
